636a34a6f9f7e00989168f5db7488cf703d288b0
cameron-poehner <cameronpoehner@gmail.com>
2020-12-22 11:27:19 -0800

    added IntersectionObserver and watcher

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('connected!');
+const terms = document.querySelector('.terms-and-conditions');
+const button = document.querySelector('.accept');
+
+function obCallback(payload) {
+    if(payload[0].intersectionRatio === 1) {
+        button.disabled = false;
+        ob.unobserve(terms.lastElementChild);
+    }
+}
+
+const ob = new IntersectionObserver(obCallback, {
+    root: terms,
+    threshold: 1
+});
+
+ob.observe(terms.lastElementChild);
